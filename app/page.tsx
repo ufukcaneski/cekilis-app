fb917a048172810f3cd31004b2707bbec60610e8
Ufukcan Eski <ufukcan.b@hotmail.com>
2025-03-09 05:22:00 +0300

    usesearchParams fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
+import { Suspense } from 'react'
 import RaffleApp from "../raffle-app"
 
 export default function SyntheticV0PageForDeployment() {
-  return <RaffleApp />
+  return (
+    <Suspense fallback={<div>Yükleniyor...</div>}>
+      <RaffleApp />
+    </Suspense>
+  )
 }
